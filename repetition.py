df468102952497e0aeed77aa0df5eb728fad6bc3
saji021198 <52283311+saji021198@users.noreply.github.com>
2019-07-22 09:58:10 -0700

    Update repetition.py

--- repetition.py
+++ repetition.py
@@ -1,13 +1,14 @@
 p=int(input())
-l=str(p)
+uu=str(p)
 c=0
-for i in range (0,len(l)):
-    for j in range(1,len(l)):
-        if(l[i]==l[j]):
+for i in range (0,len(uu)):
+    for j in range(1,len(uu)):
+        if(uu[i]==uu[j]):
             c=1
             break
         else:
             i=i+1
+    break
 if(c==1):
     print("yes")
 else:
